61e0d699f4a6f29e10a1c231fb3509b2a3f0516a
Jose Gonzalez <jozho.g@gmail.com>
2019-09-13 12:16:35 -0300

    Pequeño programa en python que determnina pares e impares en un arreglo

--- Impares.py
+++ Impares.py
@@ -1,7 +1,7 @@
 list=[2,5,28,131,1111]
 
 for i in list:
-    if i%2 == 1:
-        print(i," Es impar")
-    else:
+    if i%2 == 0:
         print(i," Es par")
+    else:
+        print(i," Es impar")
